763ce1102e333315f96180cccd325e97f4f0da8e
Harrison Grieve <rhg@alkmi.io>
2022-06-03 22:46:11 +0200

    refactor: cextract some logic

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod format;
 mod time;
 
 use std::{
+    cmp::Ordering,
     collections::HashMap,
+    fmt,
     fs::{self, Metadata, ReadDir},
+    os::unix::prelude::PermissionsExt,
     path::{Path, PathBuf},
-    process::exit, time::SystemTime, string, fmt, cmp::Ordering,
+    process::exit,
+    string,
+    time::SystemTime,
 };
 
 use rawrgs::{App, Arg};
@@ -23,7 +28,6 @@ const ONE_LINE_ARG_NAME: &str = "one-line";
 const LONG_ARG_NAME: &str = "long";
 
 // Separators
-const NEW_LINE: &str = "\n";
 const ENTRY_SPACE: &str = "  ";
 const TABLE_COL_SIZE: usize = 1;
 
@@ -45,6 +49,74 @@ struct RSEntry {
     name: String,
     path: PathBuf,
     metadata: Option<Metadata>,
+}
+
+impl RSEntry {
+    fn get_permission_string(&self) -> String {
+        let mut permission_string = String::new();
+        if let Some(file_metadata) = &self.metadata {
+            let mut permission_string_prefix = String::new();
+            if file_metadata.is_dir() {
+                permission_string_prefix.push_str("d");
+            } else if file_metadata.is_file() {
+                permission_string_prefix.push_str("-");
+            } else if file_metadata.is_symlink() {
+                permission_string_prefix.push_str("l");
+            }
+
+            let mode = file_metadata.permissions().mode();
+            let mode_string = format!("{:o}", mode);
+            let permission_bits = mode_string[mode_string.len() - 3..].to_string();
+            permission_string = String::from(permission_string_prefix);
+            for bit in permission_bits.chars() {
+                match bit {
+                    '4' => permission_string.push_str("r--"),
+                    '5' => permission_string.push_str("r-x"),
+                    '6' => permission_string.push_str("rw-"),
+                    '7' => permission_string.push_str("rwx"),
+                    _ => continue,
+                }
+            }
+        }
+        return permission_string;
+    }
+
+    fn get_file_size(&self) -> u64 {
+        if let Some(file_metadata) = &self.metadata {
+            return file_metadata.len();
+        }
+        0
+    }
+
+    fn get_table_row(&self, is_long_output: bool) -> Vec<String> {
+        let mut string_builder: Vec<String> = vec![];
+        if let Some(ref file_metadata) = &self.metadata {
+            if is_long_output {
+                let permission_string = self.get_permission_string();
+                string_builder.push(permission_string);
+
+                if let Ok(accessed) = file_metadata.modified() {
+                    let duration = accessed.duration_since(SystemTime::UNIX_EPOCH).unwrap();
+                    let days = duration.as_secs() / SECS_PER_DAY;
+                    let date = time::SimpleDate::from_days(days);
+                    string_builder.push(date.month_display(time::DateFormat::ShortMonth));
+                    string_builder.push(date.day());
+                } else {
+                    string_builder.push(String::from(" "));
+                }
+
+                let file_size = &self.get_file_size();
+                string_builder.push(file_size.to_string())
+            }
+
+            if file_metadata.is_dir() {
+                string_builder.push(format::blue_bold(&self.name))
+            } else {
+                string_builder.push(self.name.to_string());
+            }
+        }
+        string_builder
+    }
 }
 
 impl Ord for RSEntry {
@@ -73,7 +145,7 @@ impl fmt::Display for RSEntry {
             if meta.is_dir() {
                 write!(f, "{}", format::blue_bold(&self.name))?;
             } else {
-                write!(f, "{}", self.name)?;   
+                write!(f, "{}", self.name)?;
             }
         } else {
             write!(f, "{}", self.name)?;
@@ -83,8 +155,32 @@ impl fmt::Display for RSEntry {
     }
 }
 
-fn process_entries(dir: ReadDir, base_path: &Path, options: Options) -> Result<(), String> {
-    let mut dir_entries: Vec<String> = dir
+fn get_entries(dir_entries: Vec<String>, base_path: &Path) -> Vec<RSEntry> {
+    let mut rs_entries: Vec<RSEntry> = vec![];
+    for dir_entry in dir_entries {
+        let local_path = base_path.join(&dir_entry);
+        let metadata = fs::metadata(&local_path);
+        match metadata {
+            Ok(meta) => rs_entries.push(RSEntry {
+                name: dir_entry,
+                path: local_path,
+                metadata: Some(meta),
+            }),
+            Err(err) => {
+                eprintln!("{}", err);
+                rs_entries.push(RSEntry {
+                    name: dir_entry,
+                    path: local_path,
+                    metadata: None,
+                });
+            }
+        }
+    }
+    rs_entries
+}
+
+fn get_dir_entries(dir: ReadDir, options: &Options) -> Vec<String> {
+    return dir
         .into_iter()
         .filter_map(|d| d.ok())
         .map(|d| d.file_name())
@@ -97,60 +193,40 @@ fn process_entries(dir: ReadDir, base_path: &Path, options: Options) -> Result<(
             }
         })
         .collect();
+}
+
+fn get_tabular_entries(rs_entries: Vec<RSEntry>, options: &Options) -> Vec<Vec<String>> {
+    let mut output: Vec<Vec<String>> = vec![];
+    for entry in rs_entries {
+        let row = entry.get_table_row(options.is_long_output);
+        output.push(row);
+    }
+    output
+}
+
+fn process_entries(dir: ReadDir, base_path: &Path, options: Options) -> Result<(), String> {
+    let mut dir_entries = get_dir_entries(dir, &options);
 
     if options.is_show_all {
         dir_entries.push(String::from(CURRENT_DIR));
         dir_entries.push(String::from(PARENT_DIR));
     }
 
-    
-    let mut rs_entries: Vec<RSEntry> = vec![];
-    for dir_entry in dir_entries {
-        let local_path = base_path.join(&dir_entry);
-        let metadata = fs::metadata(&local_path);
-        match metadata {
-            Ok(meta) => rs_entries.push(RSEntry { name: dir_entry, path: local_path, metadata: Some(meta) }),
-            Err(err) => {
-                eprintln!("{}", err);
-                rs_entries.push(RSEntry { name: dir_entry, path: local_path, metadata: None });
-            }
-        }
-    }
-
+    let mut rs_entries = get_entries(dir_entries, base_path);
     rs_entries.sort();
-    
-    let mut output: Vec<Vec<String>> = vec![];
-    for entry in rs_entries {
-        if let Some(file_metadata) = entry.metadata {
-            let mut string_builder: Vec<String> = vec![];
-                if options.is_long_output {
-                    if let Ok(accessed) = file_metadata.accessed() {
-                        let duration = accessed.duration_since(SystemTime::UNIX_EPOCH).unwrap();
-                        let days = duration.as_secs() / SECS_PER_DAY;
-                        let date = time::SimpleDate::from_days(days);
-                        string_builder.push(date.month_display(time::DateFormat::ShortMonth));
-                        string_builder.push(date.day());
-                    } else {
-                        string_builder.push(String::from(" "));
-                    }
 
-                    string_builder.push(file_metadata.len().to_string())
-                }
-
-                if file_metadata.is_dir() {
-                    string_builder.push(format::blue_bold(&entry.name))
-                } else {
-                    string_builder.push(entry.name);
-                }
-
-                output.push(string_builder)
-        }
-    }
+    let tabular_entries = get_tabular_entries(rs_entries, &options);
 
     if options.is_one_line || options.is_long_output {
-        println!("{}", table(output, TABLE_COL_SIZE, TableAlignment::RightLastLeft).unwrap());
+        let table = table(
+            tabular_entries,
+            TABLE_COL_SIZE,
+            TableAlignment::RightLastLeft,
+        )
+        .unwrap();
+        println!("{}", table);
     } else {
-        println!("{}", output.concat().join(ENTRY_SPACE));
+        println!("{}", tabular_entries.concat().join(ENTRY_SPACE));
     }
 
     Ok(())
@@ -160,6 +236,7 @@ fn run() -> Result<(), String> {
     let app = App::new("rs")
         .about("An ls clone in rust")
         .author("Harrison Grieve")
+        .version(env!("CARGO_PKG_VERSION"))
         .arg(Arg::with_name(PATH_ARG_NAME))
         .arg(Arg::with_name(ALL_ARG_NAME).short("a"))
         .arg(Arg::with_name(ALMOST_ALL_ARG_NAME).short("A"))
@@ -189,7 +266,9 @@ fn run() -> Result<(), String> {
 
     return match fs::read_dir(base_path) {
         Ok(read_dir) => process_entries(read_dir, base_path, options),
-        Err(err) => Err(format!("rs: cannot access '{}': {}", base_path.display(), err).to_string()),
+        Err(err) => {
+            Err(format!("rs: cannot access '{}': {}", base_path.display(), err).to_string())
+        }
     };
 }
 
